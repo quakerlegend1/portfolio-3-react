8c42ef39a07a5ac99536820f340bb58f071312d9
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-03-31 18:28:43 +0300

    qq

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div className={styles.wrapper}>
       <header className={styles.header}>
         <div className={styles.leftSection}>
-          <img alt="header-logo" src=".\images\header-logo.svg"/>
+          <img alt="header-logo" src="src\images\header-logo.svg"/>
           <div>
             <h2>TASTY-BURGERS</h2>
             <p>Доставка вкусного фастфуда</p>
@@ -16,10 +16,10 @@ function App() {
           
         </div>
         <div className={styles.rightSection}>
-          <img alt="header-cart" src="./images/header-cart.svg" width="18" height="17"/>
+          <img alt="header-cart" src="src\images\header-cart.svg" width="18" height="17"/>
           <b>1300 руб.</b>
-          <img alt="header-favouriotes" src=".\images\header-favouriotes.svg" width="21" height="19"/>
-          <img alt="header-user" src=".\images\header-user.svg" width="20" height="20" />
+          <img alt="header-favouriotes" src="src\images\header-favouriotes.svg" width="21" height="19"/>
+          <img alt="header-user" src="src\images\header-user.svg" width="20" height="20" />
         </div>
       </header>
     </div>
