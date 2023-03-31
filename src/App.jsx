aef0bf0dc5612679d8a8d9fd4eb3f239f937d6fd
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-03-31 19:16:30 +0300

    qq

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 
 import styles from "./app.module.css"
 import headerLogo from "./images/header-logo.svg"
+import headerCart from "./images/header-cart.svg"
+import headerFavourites from "./images/header-favouriotes.svg"
+import headerUser from "./images/header-user.svg"
 
 function App() {
 
@@ -17,10 +20,10 @@ function App() {
           
         </div>
         <div className={styles.rightSection}>
-          <img alt="header-cart" src="..\src\images\header-cart.svg" width="18" height="17"/>
+          <img alt="header-cart" src={headerCart} width="18" height="17"/>
           <b>1300 руб.</b>
-          <img alt="header-favouriotes" src="..\src\images\header-favouriotes.svg" width="21" height="19"/>
-          <img alt="header-user" src="..\src\images\header-user.svg" width="20" height="20" />
+          <img alt="header-favouriotes" src={headerFavourites} width="21" height="19"/>
+          <img alt="header-user" src={headerUser} width="20" height="20" />
         </div>
       </header>
     </div>
