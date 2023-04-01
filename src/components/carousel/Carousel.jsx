9495bab6b43179b26037244b8d1259c0cc27d900
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-04-01 10:52:10 +0300

    ass

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,9 +1,10 @@
 import styles from "./carousel.module.css"
+import carousel_1 from "src\images\carousel-1.png"
 
 function Carousel() {
     return (
         <article className={styles.carouselMain}>
-            <img alt="carousel" src='src\images\carousel-1.png'/>
+            <img alt="carousel" src={carousel_1}/>
         </article>
     )
 }
