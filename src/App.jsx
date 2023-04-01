0d63a7b7c3fe89a8769c7b8d6b7275376c09f5ed
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-04-01 10:49:12 +0300

    1.4.2023
    qq

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
 import styles from "./app.module.css"
+import Carousel from "./components/carousel/Carousel"
 import headerLogo from "./images/header-logo.svg"
 import headerCart from "./images/header-cart.svg"
 import headerFavourites from "./images/header-favouriotes.svg"
 import headerUser from "./images/header-user.svg"
 
 function App() {
-
-
   return (
+    
     <div className={styles.wrapper}>
       <header className={styles.header}>
         <div className={styles.leftSection}>
@@ -26,7 +26,31 @@ function App() {
           <img alt="header-user" src={headerUser} width="20" height="20" />
         </div>
       </header>
+      <main className={styles.main}>
+      <Carousel/>
+        <div className={styles.search}>
+          <h2>Все товары</h2>
+          <input type="text" placeholder="Поиск..."/>
+        </div>
+        <section>
+          <ul className={styles.cardsContainer}>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+            <li>5</li>
+            <li>6</li>
+            <li>7</li>
+            <li>8</li>
+            <li>9</li>
+            <li>22</li>
+            <li>31</li>
+            <li>33</li>
+          </ul>
+        </section>
+      </main>
     </div>
+   
   )
 }
 
---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,11 @@
+import styles from "./carousel.module.css"
+
+function Carousel() {
+    return (
+        <article className={styles.carouselMain}>
+            <img alt="carousel" src='src\images\carousel-1.png'/>
+        </article>
+    )
+}
+
+export default Carousel
