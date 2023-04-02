4217e0c3509e8b16f40b838242cfeae79944a91c
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-04-02 04:52:47 +0300

    11

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,20 @@
 import styles from "./card.module.css";
 import favourite from "../../images/favourite.png"
+import burg from "../../images/1_fresh_muffin.png"
 
 function Card() {
     return (
         <li className={styles.card}>
-            <img alt="favourite" src={favourite}/>
+            <img alt="favourite" src={favourite} className={styles.favourite}/>
+            <img alt="burger" src={burg}/>
+            <p style={{textAlign:"center"}}>Сырный Фреш Маффин</p>
+            <div style={{display:"flex",justifyContent:"space-around",marginTop:"15px"}}>
+                <div className={styles.price}>
+                    <span>Цена: </span>
+                    <b>от 189 руб.</b>
+                </div>
+                <button className={styles.plusBtn}/>
+            </div>
         </li>
     )
 }
