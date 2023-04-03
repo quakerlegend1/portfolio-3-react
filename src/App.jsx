72da4db27ed821eb2d01f3d408f228f24377e6c7
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-04-03 18:09:31 +0300

    card comp + arrayCards

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,70 @@ import headerCart from "./images/header-cart.svg"
 import headerFavourites from "./images/header-favouriotes.svg"
 import headerUser from "./images/header-user.svg"
 
+const cardsArray = [
+  { id: 1,
+    title: "Фреш Маффин",
+    price: 189,
+    imageUrl: "src/images/1_fresh_muffin.png"
+    },
+    { id: 2,
+    title: "Биг Спейшил",
+    price: 189,
+    imageUrl: "src/images/2_big_special_rost.png"
+    },
+    { id: 3,
+    title: "Сырный Ролл",
+    price: 189,
+    imageUrl: "src/images/3_surnii_roll.png"
+    },
+    { id: 4,
+    title: "Биг Хит",
+    price: 189,
+    imageUrl: "src/images/4_bighit.png"
+    },
+    { id: 5,
+    title: "Чизбургер",
+    price: 69,
+    imageUrl: "src/images/5_cheeseburger.png"
+    },
+    { id: 6,
+    title: "Гамбургер",
+    price: 57,
+    imageUrl: "src/images/6_hamburger.png"
+    },
+    { id: 7,
+    title: "Чикенбургер",
+    price: 59,
+    imageUrl: "src/images/7_chikenburger.png"
+    },
+    { id: 8,
+    title: "Чикен Премьер",
+    price: 159,
+    imageUrl: "src/images/8_chikenpremier.png"
+    },
+    { id: 9,
+    title: "Цезарь Ролл",
+    price: 179,
+    imageUrl: "src/images/9_cesar_roll.png"
+    },
+    { id: 10,
+    title: "Двойной Гранд",
+    price: 253,
+    imageUrl: "src/images/10_double_grand.png"
+    },
+    { id: 11,
+    title: "Шримп Ролл",
+    price: 229,
+    imageUrl: "src/images/11_shrimp.png"
+    },
+    { id: 12,
+    title: "Фишбургер",
+    price: 215,
+    imageUrl: "src/images/12_fishburger.png"
+    }
+]
 function App() {
+  
   return (
     <div className={styles.wrapper}>
       <header className={styles.header}>
@@ -34,18 +97,7 @@ function App() {
         </div>
         <section>
           <ul className={styles.cardsContainer}>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            {cardsArray.map((card)=>{return <Card title={card.title} price={card.price} imageUrl={card.imageUrl}/>})}
           </ul>
         </section>
       </main>
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,17 @@
 import styles from "./card.module.css";
 import favourite from "../../images/favourite.png"
-import burg from "../../images/1_fresh_muffin.png"
 
-function Card() {
+
+function Card(props) {
     return (
         <li className={styles.card}>
             <img alt="favourite" src={favourite} className={styles.favourite}/>
-            <img alt="burger" src={burg}/>
-            <p style={{textAlign:"center"}}>Сырный Фреш Маффин</p>
+            <img alt="burger" src={props.imageUrl} />
+            <p style={{textAlign:"center"}}>{props.title}</p>
             <div style={{display:"flex",justifyContent:"space-around",marginTop:"15px"}}>
                 <div className={styles.price}>
                     <span>Цена: </span>
-                    <b>от 189 руб.</b>
+                    <b>от {props.price} руб.</b>
                 </div>
                 <button className={styles.plusBtn}/>
             </div>
