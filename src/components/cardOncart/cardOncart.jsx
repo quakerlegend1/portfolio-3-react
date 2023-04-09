ae9d448548d1d5a39388ec61b98a8a43ad4a754c
Armen <82414784+quakerlegend1@users.noreply.github.com>
2023-04-09 14:47:44 +0300

    asd

--- src/components/cardOncart/cardOncart.jsx
+++ src/components/cardOncart/cardOncart.jsx
@@ -14,6 +14,71 @@ function CardOncart() {
                     </div>
                     <img src="/images/close.png" alt="closeBtn" width="32" height="32" />
                 </li>
+                <li className={styles.card}>
+                    <img alt="cart-item" width="70" height="70" src={cardsArray[0].imageUrl}/>
+                    <div className={styles.textDescription}>
+                        <p>Мужские Кроссовки Nike Air Max 270</p>
+                        <b>12999 руб.</b>
+                    </div>
+                    <img src="/images/close.png" alt="closeBtn" width="32" height="32" />
+                </li>
+                <li className={styles.card}>
+                    <img alt="cart-item" width="70" height="70" src={cardsArray[0].imageUrl}/>
+                    <div className={styles.textDescription}>
+                        <p>Мужские Кроссовки Nike Air Max 270</p>
+                        <b>12999 руб.</b>
+                    </div>
+                    <img src="/images/close.png" alt="closeBtn" width="32" height="32" />
+                </li>
+                <li className={styles.card}>
+                    <img alt="cart-item" width="70" height="70" src={cardsArray[0].imageUrl}/>
+                    <div className={styles.textDescription}>
+                        <p>Мужские Кроссовки Nike Air Max 270</p>
+                        <b>12999 руб.</b>
+                    </div>
+                    <img src="/images/close.png" alt="closeBtn" width="32" height="32" />
+                </li>
+                <li className={styles.card}>
+                    <img alt="cart-item" width="70" height="70" src={cardsArray[0].imageUrl}/>
+                    <div className={styles.textDescription}>
+                        <p>Мужские Кроссовки Nike Air Max 270</p>
+                        <b>12999 руб.</b>
+                    </div>
+                    <img src="/images/close.png" alt="closeBtn" width="32" height="32" />
+                </li>
+                <li className={styles.card}>
+                    <img alt="cart-item" width="70" height="70" src={cardsArray[0].imageUrl}/>
+                    <div className={styles.textDescription}>
+                        <p>Мужские Кроссовки Nike Air Max 270</p>
+                        <b>12999 руб.</b>
+                    </div>
+                    <img src="/images/close.png" alt="closeBtn" width="32" height="32" />
+                </li>
+                <li className={styles.card}>
+                    <img alt="cart-item" width="70" height="70" src={cardsArray[0].imageUrl}/>
+                    <div className={styles.textDescription}>
+                        <p>Мужские Кроссовки Nike Air Max 270</p>
+                        <b>12999 руб.</b>
+                    </div>
+                    <img src="/images/close.png" alt="closeBtn" width="32" height="32" />
+                </li>
+                <li className={styles.card}>
+                    <img alt="cart-item" width="70" height="70" src={cardsArray[0].imageUrl}/>
+                    <div className={styles.textDescription}>
+                        <p>Мужские Кроссовки Nike Air Max 270</p>
+                        <b>12999 руб.</b>
+                    </div>
+                    <img src="/images/close.png" alt="closeBtn" width="32" height="32" />
+                </li>
+                <li className={styles.card}>
+                    <img alt="cart-item" width="70" height="70" src={cardsArray[0].imageUrl}/>
+                    <div className={styles.textDescription}>
+                        <p>Мужские Кроссовки Nike Air Max 270</p>
+                        <b>12999 руб.</b>
+                    </div>
+                    <img src="/images/close.png" alt="closeBtn" width="32" height="32" />
+                </li>
+                
             </ul>
         </div>
     )
